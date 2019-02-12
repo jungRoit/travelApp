c80d8f86577ed1d9fa8405c1f304ef0b6d359508
jungRoit <roit.jung@gmail.com>
2019-02-12 15:03:44 +0545

    data modified

--- app/screens/Home/data.js
+++ app/screens/Home/data.js
@@ -23,7 +23,41 @@
         date: 'Friday-8th February',
         images: [images.roadhouse5, images.roadhouse6, images.roadhouse4]
       }
-    ]
+    ],
+    reviews: [
+      {
+        key:'1',
+        name: 'William Scout',
+        rating: '★★★★',
+        comment: `Awesone experience it's such a beautiful to be in Boudha at evening time. Food place hospitality all are good here.`,
+        proPic: images.profilePic3,
+        addedDate: new Date('February 11, 2019 03:24:00')
+      },
+      {
+        key:'2',
+        name: 'Anonymous',
+        rating: '★★',
+        comment: `Thumbs up. it's a wonderful place to enjoy .`,
+        proPic: images.profilePic1,
+        addedDate: new Date('February 10, 2019 05:04:00')
+      },
+      {
+        key:'3',
+        name: 'Jessy Jane',
+        rating: '★★★★★',
+        comment: `Awesone experience it's such a beautiful to be in lazimpad at evening time. Food place hospitality all are good here.`,
+        proPic: images.profilePic4,
+        addedDate: new Date('February 01, 2019 01:21:00')
+      },
+      {
+        key:'4',
+        name: 'Jane Doe',
+        rating: '★★★',
+        comment: `But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure`,
+        proPic: images.profilePic2,
+        addedDate: new Date('January 11, 2019 03:24:00')
+      }
+    ]   
   },
   {
     key: '2',
@@ -38,9 +72,16 @@
       details: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint`,
       image: images.roadhouse
     },
-    pictures: {
-      images: [images.roadhouse1, images.roadhouse2, images.roadhouse3, images.roadhouse4, images.roadhouse5, images.roadhouse6,images.roadhouse1, images.roadhouse2, images.roadhouse3]
-    },
+    pictures: [
+      {
+        date: 'Saturday-9th February',
+        images: [images.roadhouse1, images.roadhouse2, images.roadhouse3]
+      },
+      {
+        date: 'Friday-8th February',
+        images: [images.roadhouse5, images.roadhouse6, images.roadhouse4]
+      }
+    ],
     reviews: [
       {
         key:'1',
@@ -92,7 +133,50 @@
     pictures: {
       images: [images.roadhouse1, images.roadhouse2, images.roadhouse3, images.roadhouse4, images.roadhouse5, images.roadhouse6,]
     }
-  },
+  },pictures: [
+    {
+      date: 'Saturday-9th February',
+      images: [images.roadhouse1, images.roadhouse2, images.roadhouse3]
+    },
+    {
+      date: 'Friday-8th February',
+      images: [images.roadhouse5, images.roadhouse6, images.roadhouse4]
+    }
+  ],
+  reviews: [
+    {
+      key:'1',
+      name: 'William Scout',
+      rating: '★★★★',
+      comment: `Awesone experience it's such a beautiful to be in Boudha at evening time. Food place hospitality all are good here.`,
+      proPic: images.profilePic3,
+      addedDate: new Date('February 11, 2019 03:24:00')
+    },
+    {
+      key:'2',
+      name: 'Anonymous',
+      rating: '★★',
+      comment: `Thumbs up. it's a wonderful place to enjoy .`,
+      proPic: images.profilePic1,
+      addedDate: new Date('February 10, 2019 05:04:00')
+    },
+    {
+      key:'3',
+      name: 'Jessy Jane',
+      rating: '★★★★★',
+      comment: `Awesone experience it's such a beautiful to be in lazimpad at evening time. Food place hospitality all are good here.`,
+      proPic: images.profilePic4,
+      addedDate: new Date('February 01, 2019 01:21:00')
+    },
+    {
+      key:'4',
+      name: 'Jane Doe',
+      rating: '★★★',
+      comment: `But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure`,
+      proPic: images.profilePic2,
+      addedDate: new Date('January 11, 2019 03:24:00')
+    }
+  ]   
   {
     key: '4',
     numberOfThings: 8,
@@ -106,9 +190,50 @@
       details: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint`,
       image: images.roadhouse
     },
-    pictures: {
-      images: [images.roadhouse1, images.roadhouse2, images.roadhouse3, images.roadhouse4, images.roadhouse5, images.roadhouse6,]
-    }
+    pictures: [
+      {
+        date: 'Saturday-9th February',
+        images: [images.roadhouse1, images.roadhouse2, images.roadhouse3]
+      },
+      {
+        date: 'Friday-8th February',
+        images: [images.roadhouse5, images.roadhouse6, images.roadhouse4]
+      }
+    ],
+    reviews: [
+      {
+        key:'1',
+        name: 'William Scout',
+        rating: '★★★★',
+        comment: `Awesone experience it's such a beautiful to be in Boudha at evening time. Food place hospitality all are good here.`,
+        proPic: images.profilePic3,
+        addedDate: new Date('February 11, 2019 03:24:00')
+      },
+      {
+        key:'2',
+        name: 'Anonymous',
+        rating: '★★',
+        comment: `Thumbs up. it's a wonderful place to enjoy .`,
+        proPic: images.profilePic1,
+        addedDate: new Date('February 10, 2019 05:04:00')
+      },
+      {
+        key:'3',
+        name: 'Jessy Jane',
+        rating: '★★★★★',
+        comment: `Awesone experience it's such a beautiful to be in lazimpad at evening time. Food place hospitality all are good here.`,
+        proPic: images.profilePic4,
+        addedDate: new Date('February 01, 2019 01:21:00')
+      },
+      {
+        key:'4',
+        name: 'Jane Doe',
+        rating: '★★★',
+        comment: `But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure`,
+        proPic: images.profilePic2,
+        addedDate: new Date('January 11, 2019 03:24:00')
+      }
+    ]   
   }
 ]
 
